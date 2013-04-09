2d05120608dc21699d89bc5162d822b0c7145248
Takeshi Ugajin <ugajintakeshishi@unknown-90-84-0d-f4-18-5b.home>
2013-04-09 12:43:24 +0100

    test2

---
+++ app.js
@@ -0,0 +1,20 @@
+Ext.Loader.setPath({
+    'Ext': 'touch/src',
+    'BanchaTouch': 'app'
+});
+
+Ext.application({
+    name: 'BanchaTouch',
+    
+    controllers: ['Main'],
+    
+    icon: 'resources/images/iTunesArtwork.png',
+    glossOnIcon: true,
+    phoneStartupScreen: 'resources/loading/phone_startup.png',
+    tabletStartupScreen: 'resources/loading/tablet_startup.png',
+
+    
+    launch: function() {
+        Ext.Viewport.add(Ext.create('BanchaTouch.view.Main'));
+    }
+});
